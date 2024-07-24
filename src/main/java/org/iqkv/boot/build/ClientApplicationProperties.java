57452d469c57a2b45c8fe121d8bd95f5ffb77205
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-07-24 12:48:46 +0300

    feat: create ClientApplicationProperties

--- src/main/java/org/iqkv/boot/build/ClientApplicationProperties.java
+++ src/main/java/org/iqkv/boot/build/ClientApplicationProperties.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("iqkv.clientApp")
+@ConfigurationProperties("iqkv.client-application")
 public record ClientApplicationProperties(@NotNull String name) {
 
   public String getName() {
