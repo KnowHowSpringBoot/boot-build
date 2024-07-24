026a61a6073a3801f4871ea6cfcca0c4baa672e8
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-07-24 12:48:08 +0300

    feat: create ClientApplicationProperties

--- src/main/java/org/iqkv/boot/build/BuildInfoConfig.java
+++ src/main/java/org/iqkv/boot/build/BuildInfoConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(ApplicationBuildInfoProperties.class)
+@EnableConfigurationProperties({ApplicationBuildInfoProperties.class, ClientApplicationProperties.class})
 public class BuildInfoConfig {
 
 }
