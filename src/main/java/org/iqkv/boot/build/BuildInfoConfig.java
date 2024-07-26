ad090686468d632856bd32150a46eb542e77e082
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-07-26 16:50:25 +0300

    fix: import ApplicationInfoContributorConfiguration
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/iqkv/boot/build/BuildInfoConfig.java
+++ src/main/java/org/iqkv/boot/build/BuildInfoConfig.java
@@ -2,9 +2,11 @@ package org.iqkv.boot.build;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableConfigurationProperties({ApplicationBuildInfoProperties.class, ClientApplicationProperties.class})
+@Import(ApplicationInfoContributorConfiguration.class)
 public class BuildInfoConfig {
 
 }
