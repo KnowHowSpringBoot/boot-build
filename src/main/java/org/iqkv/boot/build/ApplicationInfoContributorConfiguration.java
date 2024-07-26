77ce89947b326ba81ba4a9d3591749427bbda786
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-07-26 13:43:15 +0300

    feat: enable ActiveProfiles info contributor

---
+++ src/main/java/org/iqkv/boot/build/ApplicationInfoContributorConfiguration.java
@@ -0,0 +1,28 @@
+package org.iqkv.boot.build;
+
+import org.springframework.boot.actuate.autoconfigure.info.ConditionalOnEnabledInfoContributor;
+import org.springframework.boot.actuate.autoconfigure.info.InfoContributorAutoConfiguration;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@Configuration
+@AutoConfigureAfter(InfoContributorAutoConfiguration.class)
+@ConditionalOnClass(InfoContributor.class)
+public class ApplicationInfoContributorConfiguration {
+
+  /**
+   * <p>activeProfilesInfoContributor.</p>
+   *
+   * @param environment a {@link org.springframework.core.env.ConfigurableEnvironment} object.
+   * @return a {@link ActiveProfilesInfoContributor} object.
+   */
+  @Bean
+  @ConditionalOnEnabledInfoContributor("active-profiles")
+  public ActiveProfilesInfoContributor activeProfilesInfoContributor(ConfigurableEnvironment environment) {
+    return new ActiveProfilesInfoContributor(environment);
+  }
+}
