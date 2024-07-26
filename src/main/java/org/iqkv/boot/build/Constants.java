bb94edcfa3edbfeb447fc6275321cd8035b05ffb
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-07-26 13:49:42 +0300

    feat: add common app runtime constants
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

---
+++ src/main/java/org/iqkv/boot/build/Constants.java
@@ -0,0 +1,28 @@
+package org.iqkv.boot.build;
+
+/**
+ * App runtime constants.
+ */
+public final class Constants {
+
+  public static final String SYSTEM = "system";
+
+  /**
+   * Constant <code>SPRING_PROFILE_DEVELOPMENT="dev"</code>
+   */
+  public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+  /**
+   * Constant <code>SPRING_PROFILE_TEST="test"</code>
+   */
+  public static final String SPRING_PROFILE_TEST = "test";
+
+  /**
+   * Constant <code>SPRING_PROFILE_PRODUCTION="prod"</code>
+   */
+  public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+
+  private Constants() {
+  }
+}
